fb33bd0b2a6b9b2fb5e885db71c3add38ea3e123
Tithishah1901 <Radhika.shah1901@gamil.com>
2024-11-19 23:28:57 +0000

    com-orangehrmlive-sw-2-tithi-shah

---
+++ src/testsuit/ForgotPasswordTest.java
@@ -0,0 +1,59 @@
+package testsuit;
+/**
+ * Write down the following test in the
+ * ‘ForgotPasswordTest’ class
+ * 1. userShouldNavigateToForgotPasswordPage
+ * Successfully()
+ * * click on the ‘Forgot your password’ link
+ * * Verify the text ‘Reset Password’
+ * * click on the ‘Cancel’ button
+ * * Verify the text ‘Login’
+ */
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class ForgotPasswordTest extends BaseTest {
+    static String baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+    @Before
+    public void setUpBrowser() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldNavigateToForgotPasswordPageSuccessfully() {
+
+        // navigate to Forgot your password page
+        driver.findElement(By.xpath("//p[@class = 'oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
+
+        //verify the Reset Password text
+        String expectedText = "Reset Password";
+        String actualText = driver.findElement(By.xpath("//h6")).getText();
+
+        // Verify with Assert Junit test
+        Assert.assertEquals("Reset Password text not verify", expectedText, actualText);
+        
+
+        // click on the Cancel button and navigate to login page
+        driver.findElement(By.xpath("//button[@type = 'button']")).click();
+
+        //verify the Reset Password text
+        String expectedText1 = "Login";
+        String actualText1 = driver.findElement(By.xpath("//h5")).getText();
+
+        // Verify with Assert Junit test
+        Assert.assertEquals("Login text not verify", expectedText1, actualText1);
+
+    }
+
+    @After
+    public void tearDown() {
+        closeBrowser();
+    }
+}
+
